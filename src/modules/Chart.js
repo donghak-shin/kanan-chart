dc92999d73b903eec62fb1afd2c1e2bfef08a849
Donghak Shin <ehdgkr97@gmail.com>
2020-05-14 23:14:38 +0900

    클래스 내부 구조 변경

--- src/modules/Chart.js
+++ src/modules/Chart.js
@@ -36,102 +36,20 @@ export default class Chart {
             const walk = x - startX
             this.el.scrollLeft = scrollLeft - walk
         })
+    }
 
-        this.render = () => {
-            let candlesElement = this.el.getElementsByClassName('kanan-chart-candles')
-
-            if (candlesElement.length === 0) {
-                candlesElement = document.createElement('div')
-                candlesElement.className = 'kanan-chart-candles'
-                candlesElement.style.position = 'relative'
-            } else {
-                candlesElement = candlesElement[0]
-            }
-
-            this.candles.forEach((candle, i) => {
-                const el = document.createElement('div')
-                const color = candle.close > candle.open ?
-                    this.colors.rise :
-                    candle.close < candle.open ?
-                        this.colors.fall :
-                        this.colors.even
-                el.className = 'kanan-chart-candle'
-                el.style.position = 'absolute'
-                candle.el = el
-
-                const ratio = this.el.clientHeight / (this.max - this.min)
-                el.style.border = '2px solid #fff'
-                el.style.borderTop = '0 none'
-                el.style.borderBottom = '0 none'
-                el.style.backgroundColor = color
-                el.style.left = (i * 6) + 'px'
-                el.style.top = (this.max - candle.high) * ratio + 'px'
-                el.style.width = '5px'
-                el.style.height = ((candle.high - candle.low) * ratio || 1) + 'px'
-
-                const innerEl = document.createElement('div')
-                innerEl.className = 'kanan-chart-candle-inner'
-                innerEl.style.position = 'absolute'
-                innerEl.style.backgroundColor = color
-                innerEl.style.left = '-2px'
-                innerEl.style.top = (candle.high - Math.max(candle.open, candle.close)) * ratio + 'px'
-                innerEl.style.width = '5px'
-                innerEl.style.height = (Math.abs(candle.open - candle.close) * ratio || 1) + 'px'
-
-                el.appendChild(innerEl)
-                candlesElement.appendChild(el)
-            })
-
-            this.el.appendChild(candlesElement)
+    setData(data) {
+        if (!Array.isArray(data)) {
+            return Error('Data must be an array')
         }
 
-        this.setData = (data) => {
-            if (!Array.isArray(data)) {
-                return Error('Data must be an array')
-            }
+        this.min = null
+        this.max = null
 
-            this.min = null
-            this.max = null
+        const candles = []
 
-            const candles = []
-
-            data.forEach((s) => {
-                const candle = new Candle(s)
-
-                if (!this.min || this.min > candle.low) {
-                    this.min = candle.low
-                }
-
-                if (!this.max || this.max < candle.high) {
-                    this.max = candle.high
-                }
-
-                candles.push(candle)
-            })
-
-            this.candles = candles
-
-            let candlesElement = this.el.getElementsByClassName('kanan-chart-candles')
-
-            if (candlesElement.length > 0) {
-                candlesElement[0].remove()
-            }
-
-            this.render()
-            this.el.scrollLeft = this.el.scrollLeftMax
-        }
-
-        this.addPoint = (point) => {
-            if (point === undefined) {
-                return Error('Point must not be empty')
-            }
-
-            if (!point instanceof Object && !Array.isArray(point)) {
-                return Error('Point must be an object/array')
-            }
-
-            const candle = new Candle(point)
-            this.candles.push(candle)
+        data.forEach((s) => {
+            const candle = new Candle(s)
 
             if (!this.min || this.min > candle.low) {
                 this.min = candle.low
@@ -141,7 +59,89 @@ export default class Chart {
                 this.max = candle.high
             }
 
-            this.render()
+            candles.push(candle)
+        })
+
+        this.candles = candles
+
+        let candlesElement = this.el.getElementsByClassName('kanan-chart-candles')
+
+        if (candlesElement.length > 0) {
+            candlesElement[0].remove()
         }
+
+        this.render()
+        this.el.scrollLeft = this.el.scrollLeftMax
+    }
+
+    addPoint(point) {
+        if (point === undefined) {
+            return Error('Point must not be empty')
+        }
+
+        if (!point instanceof Object && !Array.isArray(point)) {
+            return Error('Point must be an object/array')
+        }
+
+        const candle = new Candle(point)
+        this.candles.push(candle)
+
+        if (!this.min || this.min > candle.low) {
+            this.min = candle.low
+        }
+
+        if (!this.max || this.max < candle.high) {
+            this.max = candle.high
+        }
+
+        this.render()
+    }
+
+    render() {
+        let candlesElement = this.el.getElementsByClassName('kanan-chart-candles')
+
+        if (candlesElement.length === 0) {
+            candlesElement = document.createElement('div')
+            candlesElement.className = 'kanan-chart-candles'
+            candlesElement.style.position = 'relative'
+        } else {
+            candlesElement = candlesElement[0]
+        }
+
+        this.candles.forEach((candle, i) => {
+            const el = document.createElement('div')
+            const color = candle.close > candle.open ?
+                this.colors.rise :
+                candle.close < candle.open ?
+                    this.colors.fall :
+                    this.colors.even
+            el.className = 'kanan-chart-candle'
+            el.style.position = 'absolute'
+            candle.el = el
+
+            const ratio = this.el.clientHeight / (this.max - this.min)
+            el.style.border = '2px solid #fff'
+            el.style.borderTop = '0 none'
+            el.style.borderBottom = '0 none'
+            el.style.backgroundColor = color
+            el.style.left = (i * 6) + 'px'
+            el.style.top = (this.max - candle.high) * ratio + 'px'
+            el.style.width = '5px'
+            el.style.height = ((candle.high - candle.low) * ratio || 1) + 'px'
+
+            const innerEl = document.createElement('div')
+            innerEl.className = 'kanan-chart-candle-inner'
+            innerEl.style.position = 'absolute'
+            innerEl.style.backgroundColor = color
+            innerEl.style.left = '-2px'
+            innerEl.style.top = (candle.high - Math.max(candle.open, candle.close)) * ratio + 'px'
+            innerEl.style.width = '5px'
+            innerEl.style.height = (Math.abs(candle.open - candle.close) * ratio || 1) + 'px'
+
+            el.appendChild(innerEl)
+            candlesElement.appendChild(el)
+        })
+
+        this.el.appendChild(candlesElement)
     }
 }
